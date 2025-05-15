4215323b123682a1913168b808937301b85c281b
chardi90 <chardi90@hotmail.com>
2025-05-15 13:29:46 +0100

    Created basic ai search function and replace limerick html function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+function publishLimerick(response) {
+  let limerickElement = document.querySelector("#limerick");
+  let limerick = response.data.answer;
+
+  limerickElement.innerHTML = `${limerick}`;
+}
+
+function generateLimerick(input) {
+  let apiKey = "a2t477eebb3f98daaa0d6cf85ob51907";
+  let prompt = `Compose a limerick using these key words: ${input}.`;
+  let context =
+    "You are a limerick poet like Edward Lear, please follow the standard limerick format to create a fabulous limerick using one key word in each line.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(publishLimerick);
+}
+
+function handleClick(event) {
+  event.preventDefault();
+  let generatorInput = document.querySelector("#generator-input");
+  let inputValue = generatorInput.value;
+
+  generateLimerick(inputValue);
+}
+
+let buttonElement = document.querySelector("#submit-button");
+buttonElement.addEventListener("Click", handleClick);
