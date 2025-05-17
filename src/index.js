e9e9b9e56d93098b2dead92ac494676017163a61
chardi90 <chardi90@hotmail.com>
2025-05-17 17:05:57 +0100

    JS removed console.log + improved SheCodes credit.

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generateLimerick(input) {
   let apiKey = "a2t477eebb3f98daaa0d6cf85ob51907";
   let prompt = `Please compose a limerick using these key words: ${input}.`;
   let context =
-    "You are a limerick poet in the style of Edward Lear, please follow the rhyme scheme (AABBA), the limerick meter, the humour and the 5 line structure to create a short and snappy limerick using one key words provided. Please generate a 5-line limerick, each line should be returned in <p></p> HTML format. Then add this line to the bottom <p>Generated on SheCodes AI.</p> with class italic.";
+    "You are a limerick poet in the style of Edward Lear, please follow the rhyme scheme (AABBA), the limerick meter, the humour and the 5 line structure to create a short and snappy limerick using one key words provided. Please generate a 5-line limerick, each line should be returned in <p></p> HTML format. Then add this line to the bottom <p>SheCodes AI</p> with class italic.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(publishLimerick);
@@ -26,7 +26,6 @@ function handleClick(event) {
   let generatorInput = document.querySelector("#generator-input");
   let inputValue = generatorInput.value;
 
-  console.log(inputValue);
   generateLimerick(inputValue);
 }
 
