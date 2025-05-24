955d52ff7e61e8af0a415ee5d790a45473cda2a8
chardi90 <chardi90@hotmail.com>
2025-05-24 07:13:06 +0100

    JS tweaks to prevent console error
    Removed useless hard HTML
    edited publish limerick function to prevent console error by adding a blank before typewriter

--- src/index.js
+++ src/index.js
@@ -12,30 +12,27 @@ const grid = document.getElementById("iconGrid");
 const total = 200;
 const formElement = document.querySelector("#generator");
 
-formElement.addEventListener("submit", handleClick);
-
 function publishLimerick(response) {
-  let limerickElement = document.querySelector("#limerick");
-  let limerick = response.data.answer;
+  const limerickElement = document.querySelector("#limerick");
+  const limerick = response.data.answer;
 
-  new Typewriter(limerickElement.innerHTML, {
-    strings: `${limerick}`,
+  limerickElement.innerHTML = "";
+
+  new Typewriter(limerickElement, {
     autoStart: true,
     delay: 1,
     cursor: null,
-  });
+  })
+    .typeString(limerick)
+    .start();
 }
 
 function generateLimerick(input) {
   let apiKey = "a2t477eebb3f98daaa0d6cf85ob51907";
   let prompt = `Please compose a limerick using these key words: ${input}.`;
   let context =
-    "You are a limerick poet in the style of Edward Lear, please follow the rhyme scheme (AABBA), the limerick meter, the humour and the 5 line structure to create a short and snappy limerick using one key words provided. Please generate a 5-line limerick, each line should be returned in <p></p> HTML format. Then add this line to the bottom <p>SheCodes AI</p> with class italic.";
+    "You are a poet like Edward Lear, please follow the rhyme scheme (AABBA), the limerick meter, the humour and the 5 line structure to create a short and snappy limerick using one key words provided. Please generate a 5-line limerick, each line should be returned in <p></p> HTML format. Then add this line to the bottom <p>SheCodes AI</p> with class italic.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  let limerickElement = document.querySelector("#limerick");
-
-  limerickElement.classList.remove("hidden");
-  limerickElement.innerHTML = `<div class="generating">⏳ Generating your limerick...</div>`;
 
   axios.get(apiUrl).then(publishLimerick);
 }
@@ -45,8 +42,9 @@ function handleClick(event) {
   let generatorInput = document.querySelector("#generator-input");
   let inputValue = generatorInput.value;
   let limerickElement = document.querySelector("#limerick");
+
   limerickElement.classList.remove("hidden");
-  limerickElement.innerHTML = "Generating your limerick...";
+  limerickElement.innerHTML = `<div class="generating">⏳ Generating your limerick...</div>`;
 
   generateLimerick(inputValue);
 }
@@ -57,3 +55,5 @@ for (let i = 0; i < total; i++) {
   span.classList.add("material-symbols-outlined");
   grid.appendChild(span);
 }
+
+formElement.addEventListener("submit", handleClick);
